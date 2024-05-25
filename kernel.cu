c0a2031154ddc0f62759aefcd804a7326a02fab3
lukas <lucasmachado292@gmail.com>
2024-05-25 18:54:32 -0300

    add boundary check

--- kernel.cu
+++ kernel.cu
@@ -8,14 +8,18 @@ __global__ void scan_kernel(float* input, float* output, float* partialSums, uns
     __shared__ float buffer2_s[BLOCK_DIM];
     float* inBuffer_s = buffer1_s;
     float* outBuffer_s = buffer2_s;
-    inbuffer_s[threadIdx.x] = input[i];
+
+    if (i < N) {
+        inBuffer_s[threadIdx.x] = input[i];
+    } else {
+        inBuffer_s[threadIdx.x] = 0.0f;
+    }
     __syncthreads();
 
     for (unsigned int stride = 1; stride <= BLOCK_DIM / 2; stride *= 2) {
         if (threadIdx.x >= stride) {
             outBuffer_s[threadIdx.x] = inBuffer_s[threadIdx.x] + inBuffer_s[threadIdx.x - stride];
-        } 
-        else {
+        } else {
             outBuffer_s[threadIdx.x] = inBuffer_s[threadIdx.x];
         }
         __syncthreads();
